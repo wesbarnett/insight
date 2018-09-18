b39f4aff3142324faee39e5a26c4f4cd529c9a4f
Wes Barnett <wes@barnett.science>
2018-09-18 09:35:11 -0400

    get model and make prediction on flask

--- flask/application/app/__init__.py
+++ flask/application/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+#TODO: probably want to restrict where requests come from
+CORS(app)
 
 from app import routes
--- flask/application/app/routes.py
+++ flask/application/app/routes.py
@@ -1,11 +1,17 @@
 from app import app
 from flask import Flask, request, jsonify
+from joblib import load
 
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
 def add_message(uuid):
     content = request.json
     print(content)
-    return jsonify({"uuid":uuid})
+    title = content['title']
+    text = content['text']
+    X = title + ' ' + text
+    clf = load('pipeline.gz')
+    prediction = clf.predict([X])[0]
+    return jsonify({"result": prediction})
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
