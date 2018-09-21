95c4fe6c3137e061a90a43155e051b5a041fa6f7
Wes Barnett <wes@barnett.science>
2018-09-21 17:48:06 -0400

    update

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,9 +1,5 @@
 // This is for the "old" Reddit format. May have to look into the new format as well.
 
-// Waits for link with ID to be clicked
-$(document).ready(function() {
-  $("#insightlink").click(handler);
-});
 
 // Handles the click. Sends the title and text to the remote server to run prediction on
 function handler() {
@@ -16,21 +12,35 @@ function handler() {
         // TODO: remove following after done with local testing
 		//url: "http://localhost:8080/api/add_message/1234",
 		dataType: "json",
-		async: false,
 		data: JSON.stringify({ "title": title, "text" : text}),
 		contentType: "application/json",
 		success: function (result) {
             // TODO: Make this a link the user can click and then populate the "choose
             // where to post" field or add link to subscribe.
-            $('#insightsuggestions').text("");
+            $('#insightsuggestions').text(" ");
             for (var i = 0; i < result.length; i++) {
-                $('#insightsuggestions').append("<ol>");
-                $('#insightsuggestions').append("<li>" + result[i] + "</li>");
-                $('#insightsuggestions').append("</ol>");
+                $('#insightsuggestions').append('<a style="font-size: small;" href="#" class="sr-suggestion" tabindex="100">' + result[i] + '</a> ');
             }
+            $('#insightlink').html('update my suggestions')
 		}
 	});
 }
 
 // Create a link with a specific ID that will be clicked
-$('#suggested-reddits').append('<h1><a href="javascript:void(0);" id="insightlink">Give me suggestions</a></h1><div id="insightsuggestions"></div>');
+$('span:contains("choose where to post")').append('<p><a href="javascript:void(0);" id="insightlink">give me suggestions</a><div id="loadingDiv" class="error">loading...</div><div id="insightsuggestions"></div></p>');
+
+$('#loadingDiv').hide();
+
+$(document)
+    // Waits for link with ID to be clicked
+    .ready(function() {
+        $("#insightlink").click(handler);
+    })
+    .ajaxStart(function () {
+        $("#loadingDiv").show();
+        $('#insightsuggestions').html("");
+    })
+    .ajaxStop(function () {
+        $("#loadingDiv").hide();
+    });
+
