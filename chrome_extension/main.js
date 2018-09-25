96b43f9c8d9dc1ff7a99af7cb353b71cc306c0c8
Wes Barnett <wes@barnett.science>
2018-09-25 11:19:55 -0400

    update appearance

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -15,19 +15,29 @@ function handler() {
             success: function (result) {
                 // TODO: Make this a link the user can click and then populate the "choose
                 // where to post" field or add link to subscribe.
-                $('#insightsuggestions').html('<p style="font-size: x-small;">subscribers &gt; 130,000</p><p>')
+                myhtml =  '<p style="font-size: x-small;">subscribers &gt; 130,000</p><p>';
                 for (var i = 0; i < 3; i++) {
-                    $('#insightsuggestions').append('<a style="font-size: small;" href="#" class="sr-suggestion" tabindex="100">' + result[i] + '</a> ');
+                    myhtml += '<a style="font-size: medium;" target="_blank" href="https://www.reddit.com/r/' + result[i] + '/submit?selftext=true&title=' + title + '&text=' + text +'" class="sr-suggestion" tabindex="100">' + result[i] + '</a>';
+                    if (i != 2) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append('</p><br><p style="font-size: x-small;">130,000 &gt; subscribers &gt; 55,000</p><p>')
+                myhtml += '</p><br><p style="font-size: x-small;">130,000 &gt; subscribers &gt; 55,000</p><p>';
                 for (var i = 3; i < 6; i++) {
-                    $('#insightsuggestions').append('<a style="font-size: small;" href="#" class="sr-suggestion" tabindex="100">' + result[i] + '</a> ');
+                    myhtml += '<a style="font-size: medium;" target="_blank" href="https://www.reddit.com/r/' + result[i] + '/submit?selftext=true&title=' + title + '&text=' + text +'" class="sr-suggestion" tabindex="100">' + result[i] + '</a>';
+                    if (i != 5) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append('</p><br><p style="font-size: x-small;">55,000 &gt; subscribers &gt; 33,000</p><p>')
+                myhtml += '</p><br><p style="font-size: x-small;">55,000 &gt; subscribers &gt; 33,000</p><p>';
                 for (var i = 6; i < result.length; i++) {
-                    $('#insightsuggestions').append('<a style="font-size: small;" href="#" class="sr-suggestion" tabindex="100">' + result[i] + '</a> ');
+                    myhtml += '<a style="font-size: medium;" target="_blank" href="https://www.reddit.com/r/' + result[i] + '/submit?selftext=true&title=' + title + '&text=' + text +'" class="sr-suggestion" tabindex="100">' + result[i] + '</a>';
+                    if (i != result.length-1) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append("</p>")
+                myhtml += '</p>';
+                $('#insightsuggestions').html(myhtml)
                 $('#insightlink').html('');
             },
             error: function(xhr, status, error) {
@@ -44,10 +54,11 @@ var string = window.location.href,
 substring0 = "submit";
 substring1 = "comments";
 
+// NEW POSTS -----------------------------------------
 if (string.indexOf(substring0) !== -1) {
 
     // Currently only works on subreddits that take self text posts
-    $('.bottom-area:first').parent().parent().append('<div class="reddit-infobar"><div style="font-size: large;">communities with content like this<span id="loadingDiv" class="error">&nbsp;&nbsp;&nbsp;loading...</span></div><div id="insightsuggestions">start typing above!</div></div>');
+    $('.bottom-area:first').parent().parent().append('<div class="reddit-infobar"><div style="font-size: large; font-weight: bold;">communities with content like this<span id="loadingDiv" class="error">&nbsp;&nbsp;&nbsp;loading...</span></div><div id="insightsuggestions">start typing above!</div></div>');
 
     $('#loadingDiv').hide();
 
@@ -62,9 +73,10 @@ if (string.indexOf(substring0) !== -1) {
     $('#title-field').find('textarea[name="title"]').bindWithDelay("keyup", handler, 100);
     $('#text-field').find('textarea[name="text"]').bindWithDelay("keyup", handler, 100);
 
+// ALREADY SUBMITTED ------------------------------------------------
 } else if (string.indexOf(substring1) !== -1) {
 
-    $('.flat-list:eq(3)').append('<div class="reddit-infobar"><div style="font-size: large;" id="insightsuggestions">communities with content like this<span class="error">&nbsp;&nbsp;&nbsp;loading...</span></div></div>');
+    $('.flat-list:eq(3)').append('<div class="reddit-infobar"><div style="font-size: large;" id="insightsuggestions"><b>communities with content like this</b><span class="error">&nbsp;&nbsp;&nbsp;loading...</span></div></div>');
     $.ajax
     ({
         type: "POST",
@@ -74,20 +86,30 @@ if (string.indexOf(substring0) !== -1) {
         contentType: "application/json",
         success: function (result) {
             if (result != "") {
-                $('#insightsuggestions').html('<div style="font-size: large;">communities with content like this</div>');
-                $('#insightsuggestions').append('<p style="font-size: x-small;">subscribers &gt; 130,000</p><p>')
+                myhtml = '<div style="font-size: large; line-height: 1.2em"><b>communities with content like this</b></div>';
+                myhtml += '<p style="font-size: x-small;">subscribers &gt; 130,000</p><p>';
                 for (var i = 0; i < 3; i++) {
-                    $('#insightsuggestions').append('<a target="_blank" style="font-size: small;" href="https://old.reddit.com/r/' + result[i] + '">' + result[i] + '</a> ');
+                    myhtml += '<a target="_blank" style="font-size: medium;" href="https://reddit.com/r/' + result[i] + '">' + result[i] + '</a> ';
+                    if (i != 2) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append('</p><br><p style="font-size: x-small;">130,000 &gt; subscribers &gt; 55,000</p><p>')
+                myhtml += '</p><br><p style="font-size: x-small;">130,000 &gt; subscribers &gt; 55,000</p><p>';
                 for (var i = 3; i < 6; i++) {
-                    $('#insightsuggestions').append('<a target="_blank" style="font-size: small;" href="https://old.reddit.com/r/' + result[i] + '">' + result[i] + '</a> ');
+                    myhtml += '<a target="_blank" style="font-size: medium;" href="https://reddit.com/r/' + result[i] + '">' + result[i] + '</a> ';
+                    if (i != 5) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append('</p><br><p style="font-size: x-small;">55,000 &gt; subscribers &gt; 33,000</p><p>')
+                myhtml += '</p><br><p style="font-size: x-small;">55,000 &gt; subscribers &gt; 33,000</p><p>';
                 for (var i = 6; i < result.length; i++) {
-                    $('#insightsuggestions').append('<a target="_blank" style="font-size: small;" href="https://old.reddit.com/r/' + result[i] + '">' + result[i] + '</a> ');
+                    myhtml += '<a target="_blank" style="font-size: medium;" href="https://reddit.com/r/' + result[i] + '">' + result[i] + '</a> ';
+                    if (i != result.length-1) {
+                        myhtml += ' &middot; '
+                    }
                 }
-                $('#insightsuggestions').append("</p>")
+                myhtml += '</p>';
+                $('#insightsuggestions').html(myhtml);
             }
         },
         error: function(xhr, status, error) {
