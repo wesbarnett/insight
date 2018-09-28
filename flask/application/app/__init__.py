df7fd3db9014b49f3f729c0ae42cad0e5e18f0d7
Wes Barnett <wes@barnett.science>
2018-09-28 18:49:51 +0000

    rm TODO

--- flask/application/app/__init__.py
+++ flask/application/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-#TODO: probably want to restrict where requests come from
-CORS(app)
+cors = CORS(app)
 
 from app import routes
